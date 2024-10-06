94acd1d6edb80592bfb3e7a384ab133e53571a63
AVC <dave@null>
2024-10-06 19:00:53 +0200

    refactor: Replace custom logging with Python's built-in logging module

--- credslayer/core/logger.py
+++ credslayer/core/logger.py
@@ -1,68 +1,17 @@
 # coding: utf-8
-from credslayer.core.session import Session
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-class Color(object):
-    BRIGHT_BLUE = "\u001b[34;1m"
-    WHITE = "\u001b[37m"
-    CYAN = "\u001b[36m"
-    RED = "\u001b[31m"
-    GREEN = "\u001b[32m"
-    RESET = "\u001b[0m"
-    BACKGROUND_RED = "\u001b[41m"
+def debug(msg, *args):
+    logging.debug(msg, *args)
 
+def info(msg, *args):
+    logging.info(msg, *args)
 
-DEBUG_MODE = False
-OUTPUT_FILE = None
+def error(msg, *args):
+    logging.error(msg, *args)
 
-
-def _log(content):
-    print(content)
-
-    if OUTPUT_FILE:
-        OUTPUT_FILE.write(content + "\n")
-
-
-def debug(*args):
-    """
-    This should be used to display information to the developers, not the users.
-    """
-
-    if not DEBUG_MODE:
-        return
-
-    if len(args) == 2:
-        session = args[0]
-        msg = args[1]
-        _log("{}[{} {}] {}{}[DEBUG]{} {}".format(Color.BRIGHT_BLUE, session.protocol, str(session), Color.RESET, Color.CYAN, Color.RESET, msg))
-    else:
-        msg = args[0]
-        _log("{}[DEBUG]{} {}".format(Color.CYAN, Color.RESET, msg))
-
-
-def info(*args):
-    """
-    This should be used to display - not necessarily - useful information to the users.
-    """
-
-    if len(args) == 2:
-        session = args[0]
-        msg = args[1]
-        _log("{}[{} {}] {}{}[INFO]{}  {}".format(Color.BRIGHT_BLUE, session.protocol, str(session), Color.RESET, Color.GREEN, Color.RESET, msg))
-    else:
-        msg = args[0]
-        _log("{}[INFO]{} {}".format(Color.GREEN, Color.RESET, msg))
-
-
-def error(msg: str):
-    """
-    This should be used when anything goes wrong (not necessarily fatal).
-    """
-    _log("{}[ERROR]{} {}".format(Color.RED, Color.RESET, msg))
-
-
-def found(session: Session, msg: str):
-    """
-    This should be used when something valuable has been found.
-    """
-    _log("{}[{} {}] {}{}{}[FOUND]{} {}".format(Color.BRIGHT_BLUE, session.protocol, str(session), Color.RESET, Color.WHITE, Color.BACKGROUND_RED, Color.RESET, msg))
+def found(session, msg):
+    logging.info("[FOUND] [{} {}] {}".format(session.protocol, str(session), msg))
