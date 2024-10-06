bcd3fd01874fba303acad61631fc655ae1ef43fc
AVC <dave@null>
2024-10-06 19:42:18 +0200

    fix logger

--- credslayer/core/logger.py
+++ credslayer/core/logger.py
@@ -6,13 +6,13 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger("CredSlayer")
 
 def debug(msg, *args):
-    logging.debug(f"{msg} {str(args)}")
+    logging.debug(f"{msg} {" | ".join(args)}")
 
 def info(msg, *args):
-    logging.info(f"{msg} {str(args)}")
+    logging.info(f"{msg} {" | ".join(args)}")
 
 def error(msg, *args):
-    logging.error(f"{msg} {str(args)}")
+    logging.error(f"{msg} {' | '.join(args)}")
 
 def found(session, msg):
     logging.info("[FOUND] [{} {}] {}".format(session.protocol, str(session), msg))
